e04e434b48831355201b2bf088f828bb3b7a4b9c
Headwipe <headwipetv@outlook.de>
2016-12-26 09:36:05 +0100

    Updated script
    - added option to select latest stable or mainline Kernel

--- kernel-helper.sh
+++ kernel-helper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\033[32mSystem updates and Tools will now be installed!\033[0m"
+echo -e "\033[32mSystem updates und Tools werden jetzt installiert!\033[0m"
 
 sleep 1
 
@@ -7,11 +7,12 @@ apt-get update && apt-get upgrade
 wait
 apt-get install -y htop kernel-package libncurses-dev wget bzip2 make build-essential dialog
 
-cmd0=(dialog --keep-tite --msgbox "You will now be asked for the Kernel Version you want to download and Compile. For the newest Kernel Version visit: https://kernel.org" 22 76 16)
 cmd=(dialog --keep-tite --menu "Select options:" 22 76 16)
 
 options=(1 "v3.0"
-         2 "v4.x")
+         2 "v4.x"
+		 3 "latest stable"
+		 4 "latest mainline")
 
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 
@@ -24,7 +25,7 @@ do
 			wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-$input1.tar.xz
             tar xvf linux-$input1.tar.xz
 			cd linux-$input1
-			echo -e "\033[32mConfigure Kernel!\033[0m"
+			echo -e "\033[32mKernel wird konfiguriert!\033[0m"
 			
 			sleep 1
 			
@@ -33,8 +34,8 @@ do
 			
 			cmd2=(dialog --keep-tite --menu "Select Options:" 22 76 16)
 			
-			options2=(1 "1000Hz LLD"
-					  2 "100Hz FPD")
+			options2=(1 "1000Hz LLD (Low-Latency Desktop)"
+					  2 "100Hz FPD (Forced P. Desktop)")
 					  
 			choices2=$("${cmd2[@]}" "${options2[@]}" 2>&1 >/dev/tty)
 			
@@ -48,7 +49,7 @@ do
 						rm-r linux-$input1*
 						rm /tmp/gk-v.tmp
 						
-						dialog --title "Message" --msgbox "Compile complete!! The Kernel Files are in the same Folder as this script!!" 8 30
+						dialog --title "Message" --msgbox "Compile complete!!" 8 30
 						
 						clear
 						break
@@ -60,7 +61,7 @@ do
 						rm-r linux-$input1*
 						rm /tmp/gk-v.tmp
 						
-						dialog --title "Message" --msgbox "Compile complete!! The Kernel Files are in the same Folder as this script!!" 8 30
+						dialog --title "Message" --msgbox "Compile complete!!" 8 30
 						
 						clear
 						break
@@ -74,7 +75,7 @@ do
 			wget https://www.kernel.org/pub/linux/kernel/v4.x/linux-$input1.tar.xz
             tar xvf linux-$input1.tar.xz
 			cd linux-$input1
-			echo -e "\033[32mConfiguring Kernel!\033[0m"
+			echo -e "\033[32mconfiguring Kernel!\033[0m"
 			
 			sleep 1
 			
@@ -83,8 +84,8 @@ do
 			
 			cmd3=(dialog --keep-tite --menu "Select Options:" 22 76 16)
 			
-			options3=(1 "1000Hz LLD"
-					  2 "100Hz FPD")
+			options2=(1 "1000Hz LLD (Low-Latency Desktop)"
+					  2 "100Hz FPD (Forced P. Desktop)")
 					  
 			choices3=$("${cmd3[@]}" "${options3[@]}" 2>&1 >/dev/tty)
 			
@@ -92,25 +93,25 @@ do
 			do
 				case $choice3 in
 					1)
-						echo -e "\033[32mKernel wird compiled!\033[0m"
+						echo -e "\033[32mcompiling Kernel!\033[0m"
 						make-kpkg clean && make-kpkg -j4 --append-to-version -gk-1k-lld --initrd kernel_image kernel_headers
 						
-						rm -r linux-$input1*
+						rm-r linux-$input1*
 						rm /tmp/gk-v.tmp
 						
-						dialog --title "Message" --msgbox "Compile complete!! The Kernel Files are in the same Folder as this script!!" 8 30
+						dialog --title "Message" --msgbox "Compile complete!!" 8 30
 						
 						clear
 						break
 						;;
 					2)
-						echo -e "\033[32mKernel wird compiled!\033[0m"
+						echo -e "\033[32mcompiling Kernel!\033[0m"
 						make-kpkg clean && make-kpkg -j4 --append-to-version -gk-100-FPD --initrd kernel_image kernel_headers
 						
-						rm -r linux-$input1*
+						rm-r linux-$input1*
 						rm /tmp/gk-v.tmp
 						
-						dialog --title "Message" --msgbox "Compile complete!! Die Dateien findest du im Root Verzeichnis dieses Scriptes!!" 8 30
+						dialog --title "Message" --msgbox "Compile complete!!" 8 30
 						
 						clear
 						break
@@ -118,5 +119,105 @@ do
 				esac
 			done			
 			;;
-    esac
+		3)
+			curl -s -k https://www.kernel.org/feeds/kdist.xml | sed -n -e 's@.*<guid>\(.*\)</guid>.*@\1@p' | grep 'stable' | head -1 | awk -F , '{print $3}'>/tmp/gk-v.tmp
+			input1="$(cat /tmp/gk-v.tmp)"
+			wget https://www.kernel.org/pub/linux/kernel/v4.x/linux-$input1.tar.xz
+            tar xvf linux-$input1.tar.xz
+			cd linux-$input1
+			echo -e "\033[32mcompiling Kernel!\033[0m"
+			
+			sleep 1
+			
+			make localyesconfig
+			make menuconfig
+			
+			cmd2=(dialog --keep-tite --menu "Select Options:" 22 76 16)
+			
+			options2=(1 "1000Hz LLD (Low-Latency Desktop)"
+					  2 "100Hz FPD (Forced P. Desktop)")
+					  
+			choices2=$("${cmd2[@]}" "${options2[@]}" 2>&1 >/dev/tty)
+			
+			for choice2 in $choices2
+			do
+				case $choice2 in
+					1)
+						echo -e "\033[32mcompiling Kernel!\033[0m"
+						make-kpkg clean && make-kpkg -j4 --append-to-version -gk-1k-lld --initrd kernel_image kernel_headers
+						
+						rm-r linux-$input1*
+						rm /tmp/gk-v.tmp
+						
+						dialog --title "Message" --msgbox "Compile complete!!" 8 30
+						
+						clear
+						break
+						;;
+					2)
+						echo -e "\033[32mcompiling Kernel!\033[0m"
+						make-kpkg clean && make-kpkg -j4 --append-to-version -gk-100-FPD --initrd kernel_image kernel_headers
+						
+						rm-r linux-$input1*
+						rm /tmp/gk-v.tmp
+						
+						dialog --title "Message" --msgbox "Compile complete!!" 8 30
+						
+						clear
+						break
+						;;
+				esac
+			done			
+			;;
+		4)
+			curl -s -k https://www.kernel.org/feeds/kdist.xml | sed -n -e 's@.*<guid>\(.*\)</guid>.*@\1@p' | grep 'mainline' | head -1 | awk -F , '{print $3}'>/tmp/gk-v.tmp
+			input1="$(cat /tmp/gk-v.tmp)"
+			wget https://www.kernel.org/pub/linux/kernel/v4.x/testing/linux-$input1.tar.xz
+            tar xvf linux-$input1.tar.xz
+			cd linux-$input1
+						echo -e "\033[32mcompiling Kernel!\033[0m"
+			
+			sleep 1
+			
+			make localyesconfig
+			make menuconfig
+			
+			cmd2=(dialog --keep-tite --menu "Select Options:" 22 76 16)
+			
+			options2=(1 "1000Hz LLD (Low-Latency Desktop)"
+					  2 "100Hz FPD (Forced P. Desktop)")
+					  
+			choices2=$("${cmd2[@]}" "${options2[@]}" 2>&1 >/dev/tty)
+			
+			for choice2 in $choices2
+			do
+				case $choice2 in
+					1)
+						echo -e "\033[32mcompiling Kernel!\033[0m"
+						make-kpkg clean && make-kpkg -j4 --append-to-version -gk-1k-lld --initrd kernel_image kernel_headers
+						
+						rm-r linux-$input1*
+						rm /tmp/gk-v.tmp
+						
+						dialog --title "Message" --msgbox "Compile complete!!" 8 30
+						
+						clear
+						break
+						;;
+					2)
+						echo -e "\033[32mcompiling Kernel!\033[0m"
+						make-kpkg clean && make-kpkg -j4 --append-to-version -gk-100-FPD --initrd kernel_image kernel_headers
+						
+						rm-r linux-$input1*
+						rm /tmp/gk-v.tmp
+						
+						dialog --title "Message" --msgbox "Compile complete!!" 8 30
+						
+						clear
+						break
+						;;
+				esac
+			done			
+			;;
+	esac
 done
